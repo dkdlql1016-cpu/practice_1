c4deac62c3a4e1174c56f293a620acf0673c4629
Kim <ckim19@kr.kpmg.com>
2026-03-18 17:43:17 +0900

    Set up Selenium scraping phase

--- read_excel.py
+++ read_excel.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
 file_path = "sample.xlsx"
-df = pd.read_excel(file_path)
 
+excel_file = pd.ExcelFile(file_path)
+df = pd.read_excel(excel_file,sheet_name="FY25_결과", header=1)
+
+print("===head===")
 print(df.head())
+print("\n===columns===")
+print(df.columns)
+print("\n===info===")
+print(df.info())
