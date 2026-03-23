fd48a91238c187de69c50c2910915c8a83c5bd49
Kim <ckim19@kr.kpmg.com>
2026-03-23 16:47:14 +0900

    add login page automation

--- scrape_to_excel.py
+++ scrape_to_excel.py
@@ -1,3 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()
+LOGIN_URL = "https://mindbridge-ema.kpmg.com/idp/login"
+MAX_WAIT_TIME = 10
+LOGIN_SELECTOR = "a[href='idp/oauth2/authorize/microsoft']"
+
+driver = webdriver.Edge()
+wait = WebDriverWait(driver, MAX_WAIT_TIME)
+
+driver.get(LOGIN_URL)
+
+login_link = wait.until(
+    EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, LOGIN_SELECTOR)
+    )
+)
+
+login_link.click()
+
+input("Press enter to continue...")
+driver.quit()
