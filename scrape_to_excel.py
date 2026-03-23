2d162d7e3a9fb0a34c7f57c3c0ff0944c4138c16
Kim <ckim19@kr.kpmg.com>
2026-03-23 17:54:25 +0900

    login

--- scrape_to_excel.py
+++ scrape_to_excel.py
@@ -5,18 +5,22 @@ from selenium.webdriver.support import expected_conditions as EC
 
 LOGIN_URL = "https://mindbridge-ema.kpmg.com/idp/login"
 MAX_WAIT_TIME = 10
-LOGIN_SELECTOR = "a[href='idp/oauth2/authorize/microsoft']"
+LOGIN_SELECTOR = "a[href*='microsoft']"
+
 
 driver = webdriver.Edge()
 wait = WebDriverWait(driver, MAX_WAIT_TIME)
 
 driver.get(LOGIN_URL)
 
-login_link = wait.until(
+links = wait.until(
     EC.element_to_be_clickable(
-        (By.CSS_SELECTOR, LOGIN_SELECTOR)
+    find_element(
+        By.CSS_SELECTOR, LOGIN_SELECTOR)
     )
 )
+
+print("찾은갯수", len(links))
 
 login_link.click()
 
